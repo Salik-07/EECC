cf3dabb798ea2841c931356d33ce451f9635e24a
Salik-07 <salikhussain41@gmail.com>
2024-11-10 20:37:01 +0500

    Setup express application

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Page');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
